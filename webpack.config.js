fc27276cb0d223bab93a8d11c949faa1fd627063
undancer <undancer@gmail.com>
2020-09-03 07:10:44 +0800

    将项目转为webpack打包的node项目

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new CopyPlugin({
+        //     patterns: [{from: 'src/index.html'}],
+        // }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: true
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin()
+    ]
+};
+
+module.exports = config;
