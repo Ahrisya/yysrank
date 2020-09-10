6c60b777119525ca72d3030801c9094be8dfbcff
undancer <undancer@gmail.com>
2020-09-10 12:40:33 +0800

    点击查询时显示数据

--- src/components/WinRate.vue
+++ src/components/WinRate.vue
@@ -1,75 +1,91 @@
 <template>
-  <div>
-    <!--    <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="handleSubmit">-->
-    <!--      <a-form-item label="Note">-->
-    <!--        <a-input-->
-    <!--            v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"-->
-    <!--        />-->
-    <!--      </a-form-item>-->
-    <!--      <a-form-item label="Gender">-->
-    <!--        <a-select-->
-    <!--            v-decorator="[-->
-    <!--          'gender',-->
-    <!--          { rules: [{ required: true, message: 'Please select your gender!' }] },-->
-    <!--        ]"-->
-    <!--            placeholder="Select a option and change input text above"-->
-    <!--            @change="handleSelectChange"-->
-    <!--        >-->
-    <!--          <a-select-option value="male">-->
-    <!--            male-->
-    <!--          </a-select-option>-->
-    <!--          <a-select-option value="female">-->
-    <!--            female-->
-    <!--          </a-select-option>-->
-    <!--        </a-select>-->
-    <!--      </a-form-item>-->
-    <!--      <a-form-item :wrapper-col="{ span: 12, offset: 5 }">-->
-    <!--        <a-button type="primary" html-type="submit">-->
-    <!--          Submit-->
-    <!--        </a-button>-->
-    <!--      </a-form-item>-->
-    <!--    </a-form>-->
-    <div id="components-form-demo-advanced-search">
-      <a-form :form="form" layout="horizontal" @submit="handleSearch">
-        <!-- ban 位 -->
-        <a-row>
-          <a-col :span="12">
-            <a-form-item :label-col="{ span: 4 }" :wrapper-col="{ span:8 }" label="我方ban位">
-              <HeroSelect v-decorator="[`this_ban`]" placeholder="请选择式神..."/>
-            </a-form-item>
-          </a-col>
-          <a-col :span="12">
-            <a-form-item :label-col="{ span: 4 }" :wrapper-col="{ span:8 }" label="敌方ban位">
-              <HeroSelect v-decorator="[`that_ban`]" placeholder="请选择式神..."/>
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <!-- 五个可选位置 -->
-        <a-row v-for="i in 5" :key="i">
-          <a-col :span="12">
-            <a-form-item :label="`我方${i}号位`" :label-col="{ span: 4 }" :wrapper-col="{ span:8 }">
-              <HeroSelect v-decorator="[`this_${i}`]" placeholder="请选择式神..."/>
-            </a-form-item>
-          </a-col>
-          <a-col :span="12">
-            <a-form-item :label="`敌方${i}号位`" :label-col="{ span: 4 }" :wrapper-col="{ span:8 }">
-              <HeroSelect v-decorator="[`that_${i}`]" placeholder="请选择式神..."/>
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row>
-          <a-col :span="24" :style="{ textAlign: 'center' }">
-            <a-button html-type="submit" type="primary">
-              Search
-            </a-button>
-          </a-col>
-        </a-row>
-      </a-form>
-      <div class="search-result-list">
-        Search Result List
-      </div>
-    </div>
+  <div id="components-form-demo-advanced-search">
+    <a-form :form="form" layout="horizontal" @submit="handleSearch">
+      <!-- ban 位 -->
+      <a-row>
+        <a-col :span="12">
+          <a-form-item :label-col="{ span: 4 }" :wrapper-col="{ span:8 }" label="我方ban位">
+            <HeroSelect v-decorator="[`this_ban`]" placeholder="请选择式神..."/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item :label-col="{ span: 4 }" :wrapper-col="{ span:8 }" label="敌方ban位">
+            <HeroSelect v-decorator="[`that_ban`]" placeholder="请选择式神..."/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <!-- 五个可选位置 -->
+      <a-row v-for="i in 5" :key="i">
+        <a-col :span="12">
+          <a-form-item :label="`我方${i}号位`" :label-col="{ span: 4 }" :wrapper-col="{ span:8 }">
+            <HeroSelect v-decorator="[`this_${i}`]" placeholder="请选择式神..."/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item :label="`敌方${i}号位`" :label-col="{ span: 4 }" :wrapper-col="{ span:8 }">
+            <HeroSelect v-decorator="[`that_${i}`]" placeholder="请选择式神..."/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row>
+        <a-col :span="24" :style="{ textAlign: 'center' }">
+          <a-button html-type="submit" type="primary">
+            Search
+          </a-button>
+        </a-col>
+      </a-row>
+    </a-form>
+    <a-modal
+        width="80%"
+        title="查询结果"
+        :visible="visible"
+        :confirm-loading="confirmLoading"
+        @ok="handleOk"
+        @cancel="handleCancel"
+    >
+      <a-table :data-source="data">
+        <a-table-column title="我方阵容">
+          <template v-slot="{thisTeam}">
+            <HeroIcon :id="Number.parseInt(team)" v-for="(team,index) in thisTeam" :key="index"
+                      style="margin: 4px"/>
+          </template>
+        </a-table-column>
+        <a-table-column title="对方阵容">
+          <template v-slot="{thatTeam}">
+            <HeroIcon :id="Number.parseInt(team)" v-for="(team,index) in thatTeam" :key="index"
+                      style="margin: 4px"/>
+          </template>
+        </a-table-column>
+        <a-table-column title="胜场" colSpan="1" dataIndex="w" :sorter="(a,b) => a.w - b.w">
+        </a-table-column>
+        <a-table-column title="总场次" colSpan="1" dataIndex="s" :sorter="(a,b) => a.s - b.s">
+        </a-table-column>
+        <a-table-column title="胜率" colSpan="1">
+          <template v-slot="{w,s}">
+            {{ (100 * w / s).toFixed(2) }} %
+          </template>
+        </a-table-column>
+        <a slot="name" slot-scope="text">{{ text }}</a>
+        <span slot="customTitle"><a-icon type="smile-o"/> Name</span>
+        <span slot="tags" slot-scope="tags">
+      <a-tag
+          v-for="tag in tags"
+          :key="tag"
+          :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+      >
+        {{ tag.toUpperCase() }}
+      </a-tag>
+    </span>
+        <span slot="action" slot-scope="text, record">
+      <a>Invite 一 {{ record.name }}</a>
+      <a-divider type="vertical"/>
+      <a>Delete</a>
+      <a-divider type="vertical"/>
+      <a class="ant-dropdown-link"> More actions <a-icon type="down"/> </a>
+    </span>
+      </a-table>
 
+    </a-modal>
   </div>
 </template>
 
@@ -78,10 +94,11 @@ import HeroSelect from "@/components/HeroSelect";
 import battleTable from "@/data/data.json"
 import * as _ from "underscore";
 import isContained from "@/utils/arrays";
+import HeroIcon from "@/components/HeroIcon";
 
 export default {
   name: "WinRate",
-  components: {HeroSelect},
+  components: {HeroIcon, HeroSelect},
   props: ['title'],
   // data() {
   //   return {
@@ -109,6 +126,10 @@ export default {
     return {
       expand: false,
       form: this.$form.createForm(this, {name: 'advanced_search'}),
+      ModalText: 'Content of the modal',
+      visible: false,
+      confirmLoading: false,
+      data: {},
     };
   },
   computed: {
@@ -121,6 +142,29 @@ export default {
   },
   methods: {
     handleSearch(e) {
+      e.preventDefault();
+      console.log("!!");
+      this.handleSearch1(e);
+      this.showModal();
+    },
+    showModal() {
+      this.visible = true;
+    },
+    handleOk() {
+      this.ModalText = 'The modal will be closed after two seconds';
+      this.confirmLoading = true;
+      setTimeout(() => {
+        this.visible = false;
+        this.confirmLoading = false;
+      }, 2000);
+    },
+    handleCancel() {
+      console.log('Clicked cancel button');
+      this.visible = false;
+    },
+
+
+    handleSearch1(e) {
       e.preventDefault();
       // eslint-disable-next-line @typescript-eslint/camelcase
       const {this_ban, this_1, this_2, this_3, this_4, this_5, that_ban, that_1, that_2, that_3, that_4, that_5} = this.form.getFieldsValue();
@@ -176,7 +220,9 @@ export default {
               if (/^\d{2,3}(_\d{2,3}){9}$/.test(key)) {
                 const value = prev[key] || {s: 0, w: 0, l: 0};
                 value.key = key;
-                value.team = _.chunk(key.split('_'), 5);
+                const teams = _.chunk(key.split('_'), 5);
+                value.thisTeam = _.first(teams);
+                value.thatTeam = _.last(teams);
                 value.s = value.s + next[key].s;
                 value.w = value.w + next[key].w;
                 value.l = value.l + next[key].l;
@@ -186,7 +232,8 @@ export default {
             return prev;
           })
       // console.log(reports);
-      console.log(Object.values(results));
+      this.data = Object.values(results);
+      // console.log(Object.values(results));
     },
 
     handleReset() {
