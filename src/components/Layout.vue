b5a408ac90b75827e2b9e0bd0bc4656df76ae4f3
undancer <undancer@gmail.com>
2020-09-21 07:45:07 +0800

    菜单选择随路由而改变

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,110 +3,54 @@
     <a-layout-header class="header" style="padding: 0px">
       <div class="logo"/>
       <a-menu
-          :default-selected-keys="['2']"
+          :selected-keys="[activeRoute]"
           :style="{ lineHeight: '64px' }"
           mode="horizontal"
           theme="dark"
       >
-        <a-menu-item key="1">
+        <a-menu-item key="/win-rate">
           <router-link to="/win-rate">
             阵容胜率排行
           </router-link>
         </a-menu-item>
-        <a-menu-item key="2">
+        <a-menu-item key="/rank">
           <router-link to="/rank">
             排行榜
           </router-link>
         </a-menu-item>
-        <a-menu-item key="3">
+        <a-menu-item key="/recommend">
           <router-link to="/recommend">
             模拟翻牌
           </router-link>
         </a-menu-item>
-        <a-menu-item key="4">
+        <a-menu-item key="/more-tools">
           <router-link to="/more-tools">
             攻略&工具导航
           </router-link>
         </a-menu-item>
-        <a-menu-item key="5">
+        <a-menu-item key="/tutorial">
           <router-link to="/tutorial">
             食用指南
           </router-link>
         </a-menu-item>
       </a-menu>
     </a-layout-header>
-    <a-layout>
-      <!--      <a-layout-sider style="background: #fff" width="200">-->
-      <!--        <a-menu-->
-      <!--            :default-open-keys="['sub1']"-->
-      <!--            :default-selected-keys="['1']"-->
-      <!--            :style="{ height: '100%', borderRight: 0 }"-->
-      <!--            mode="inline"-->
-      <!--        >-->
-      <!--          <a-sub-menu key="sub1">-->
-      <!--            <span slot="title"><a-icon type="user"/>subnav 1</span>-->
-      <!--            <a-menu-item key="1">-->
-      <!--              option1-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="2">-->
-      <!--              option2-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="3">-->
-      <!--              option3-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="4">-->
-      <!--              option4-->
-      <!--            </a-menu-item>-->
-      <!--          </a-sub-menu>-->
-      <!--          <a-sub-menu key="sub2">-->
-      <!--            <span slot="title"><a-icon type="laptop"/>subnav 2</span>-->
-      <!--            <a-menu-item key="5">-->
-      <!--              option5-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="6">-->
-      <!--              option6-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="7">-->
-      <!--              option7-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="8">-->
-      <!--              option8-->
-      <!--            </a-menu-item>-->
-      <!--          </a-sub-menu>-->
-      <!--          <a-sub-menu key="sub3">-->
-      <!--            <span slot="title"><a-icon type="notification"/>subnav 3</span>-->
-      <!--            <a-menu-item key="9">-->
-      <!--              option9-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="10">-->
-      <!--              option10-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="11">-->
-      <!--              option11-->
-      <!--            </a-menu-item>-->
-      <!--            <a-menu-item key="12">-->
-      <!--              option12-->
-      <!--            </a-menu-item>-->
-      <!--          </a-sub-menu>-->
-      <!--        </a-menu>-->
-      <!--      </a-layout-sider>-->
-      <a-layout style="padding: 0 24px 24px">
-        <a-page-header
-            :sub-title="subTitle"
-            :title="title"
-        />
-        <!--        <a-page-header-->
-        <!--            :title="title"-->
-        <!--            :sub-title="subTitle"-->
-        <!--            :breadcrumb="{ props: { routes } }"-->
-        <!--        />-->
+    <a-layout style="padding: 0 24px 24px">
+      <a-page-header
+          :sub-title="subTitle"
+          :title="title"
+      />
+      <!--        <a-page-header-->
+      <!--            :title="title"-->
+      <!--            :sub-title="subTitle"-->
+      <!--            :breadcrumb="{ props: { routes } }"-->
+      <!--        />-->
 
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          <router-view></router-view>
-        </a-layout-content>
-      </a-layout>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <router-view></router-view>
+      </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
@@ -128,12 +72,14 @@ export default {
       if (subTitle !== undefined) {
         this.subTitle = subTitle;
       }
+      this.activeRoute = this.$route.fullPath;
     },
   },
   data() {
     return {
       collapsed: false,
       title: 'title',
+      activeRoute: '',
       subTitle: 'sub-title',
       // routes: [
       //   {
@@ -156,7 +102,7 @@ export default {
 
 <style scoped>
 
-#components-layout-demo-top-side-2 .logo {
+.logo {
   /*width: 120px;*/
   width: 200px;
   height: 64px;
