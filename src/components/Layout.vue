790ffc07e3824e8a5178856212b80562dc327cba
undancer <undancer@gmail.com>
2020-09-21 10:47:31 +0800

    当数据更新时提示给用户

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -50,17 +50,43 @@
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
         <router-view></router-view>
+
       </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 
 <script>
+import config from "@/config";
+import battleTable from "../../data/data.json";
+import moment from "moment";
+
 export default {
   name: "Layout",
   watch: {
     $route() {
       this.refreshTitle();
+    }
+  },
+  mounted() {
+    if (this.$cookies.get('last_updated') !== config.update) {
+      const handleOk = () => {
+        this.$cookies.set('last_updated', config.update, moment().add(1000, 'days').toDate())
+      }
+      const h = this.$createElement;
+      this.$info({
+        width: '36%',
+        maskClosable: true,
+        title: 'Update',
+        content: h('div', {}, [
+          h('h3', config.update),
+          h('ul', {}, [
+            h('li', '数据更新：更新了09/07-09/13式神排行榜及' + battleTable.data.length + '场斗技数据。')
+          ]),
+        ]),
+        onOk: handleOk,
+        onCancel: handleOk,
+      });
     }
   },
   methods: {
@@ -75,6 +101,7 @@ export default {
       this.activeRoute = this.$route.fullPath;
     },
   },
+
   data() {
     return {
       collapsed: false,
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Antd from 'ant-design-vue';
 import App from "@/App.vue";
 import router from "@/router";
+import VueCookies from 'vue-cookies';
 
 Vue.config.productionTip = false
 
 console.log('vue init');
 
 Vue.use(Antd);
+Vue.use(VueCookies)
 
 new Vue({
     render: h => h(App),
