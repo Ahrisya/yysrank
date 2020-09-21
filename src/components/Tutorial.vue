e081c078da18671739aacede3b657198835a719f
undancer <undancer@gmail.com>
2020-09-21 09:28:23 +0800

    文字同步

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -75,6 +75,16 @@
           </li>
         </ul>
       </a-collapse-panel>
+      <a-collapse-panel key="4" header="Acknowledgement">
+        <ul>
+          <li>
+            感谢<a href="https://undancer.com/">@米粒</a>的代码与结构优化。
+          </li>
+          <li>
+            感谢@老铁扎心了的攻略整理。
+          </li>
+        </ul>
+      </a-collapse-panel>
     </a-collapse>
   </div>
 </template>
